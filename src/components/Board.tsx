149e03552541a8c1e033aece5aab904af5ac44ea
Petya29 <petya29avramenko@gmail.com>
2023-05-06 15:48:09 +0200

    Prepare main form

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,5 +1,20 @@
+import { List } from "./partials/List";
+import { Divider } from "./ui/data-display";
+import { Container } from "./ui/layout";
+import { Paper } from "./ui/surfaces";
+
 export const Board = () => {
-  return (
-    <div>Board</div>
-  )
+    return (
+        <Container
+            className="mt-12 w-full mx-4"
+            maxWidth="md"
+            disableGutters
+        >
+            <Paper className="w-full">
+                <div className="text-center text-2xl divide-x-2">User Story</div>
+                <Divider className="my-4" />
+                <div><List /></div>
+            </Paper>
+        </Container>
+    )
 }
